7eefc9775d990364e490a3d91fac5d2b2c872ac9
andrea pradarelli <andpra70@gmail.com>
2020-06-13 21:47:57 +0200

    fix

--- build.sh
+++ build.sh
@@ -6,4 +6,10 @@ docker kill ytdl
 
 docker build . -t ytdl
 
-docker run --name ytdl --rm -v $PWD/downloads:/ytdl/downloads -p 3000:3000/tcp ytdl
+docker tag ytdl andpra70/ytdl
+
+docker login andpra70
+docker push andpra70/ytdl
+
+#docker run --name ytdl --rm -v $PWD/downloads:/ytdl/downloads -p 3000:3000/tcp ytdl
+docker run --name ytdl --rm -v $PWD/downloads:/ytdl/downloads -p 3000:3000/tcp andpra70/ytdl
